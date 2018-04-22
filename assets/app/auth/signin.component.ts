05a3123b9bc0538f51163ef7b3e5062e468ba2b0
AnkitVartak1993 <ankitvartak@gmail.com>
2018-04-22 18:56:45 -0400

    Added signup forms with data driven validators

--- assets/app/auth/signin.component.ts
+++ assets/app/auth/signin.component.ts
@@ -6,5 +6,5 @@ import { Component } from "@angular/core";
     templateUrl: './signin.component.html'
 })
 export class SigninComponent{
-
+    
 }
--- assets/app/auth/signup.component.ts
+++ assets/app/auth/signup.component.ts
@@ -1,9 +1,26 @@
-import { Component } from "@angular/core";
+import { Component,  OnInit } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 
 @Component({
     selector: 'app-signup',
     templateUrl: './signup.component.html'
 })
-export class SignupComponent{
+export class SignupComponent implements OnInit{
+signUpForm: FormGroup;
 
+    onSubmit(){
+        console.log('form submitted');
+        this.signUpForm.reset();
+    }
+    ngOnInit() {
+        this.signUpForm = new FormGroup({
+            firstName: new FormControl(null, Validators.required),
+            lastName: new FormControl(null, Validators.required),
+            email: new FormControl(null, [
+                Validators.required,
+                Validators.email
+               ]),
+            password: new FormControl(null, Validators.required)
+        });
+    }
 }
