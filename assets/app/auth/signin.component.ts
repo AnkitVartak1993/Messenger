f31bfbd4369d4b4f91b470378057417b0c3414f6
AnkitVartak1993 <ankitvartak@gmail.com>
2018-04-22 22:12:54 -0400

    Added Sign in and delete option for messages

--- assets/app/auth/signin.component.ts
+++ assets/app/auth/signin.component.ts
@@ -1,10 +1,24 @@
-
-import { Component } from "@angular/core";
+import { Component,  OnInit } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 
 @Component({
     selector: 'app-signin',
     templateUrl: './signin.component.html'
 })
-export class SigninComponent{
-    
+export class SigninComponent implements OnInit{
+signInForm: FormGroup;
+
+    onSubmit(){
+        console.log('form submitted');
+        this.signInForm.reset();
+    }
+    ngOnInit() {
+        this.signInForm = new FormGroup({
+            email: new FormControl(null, [
+                Validators.required,
+                Validators.email
+               ]),
+            password: new FormControl(null, Validators.required)
+        });
+    }
 }
