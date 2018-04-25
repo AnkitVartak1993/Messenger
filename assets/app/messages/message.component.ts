05df0fc2fb979cc1e97bcc3ee8a6499d09a7a425
AnkitVartak1993 <ankitvartak@gmail.com>
2018-04-24 23:34:32 -0400

    Added Delete message route

--- assets/app/messages/message.component.ts
+++ assets/app/messages/message.component.ts
@@ -31,6 +31,9 @@ constructor( private messageService: MessageService ){
     this.messageService.editMessage(this.message);
  }
  onDelete(){
-this.messageService.deleteMessage(this.message);
+    this.messageService.deleteMessage(this.message)
+        .subscribe(
+            result => console.log(result)
+        );
  }
 }
--- assets/app/messages/message.service.ts
+++ assets/app/messages/message.service.ts
@@ -51,5 +51,8 @@ getMessage(){
 
 deleteMessage(message: Message){
     this.messages.splice(this.messages.indexOf(message),1);
+    return this.http.delete('http://localhost:3000/message/' + message.messageId)
+            .map((response: Response) => response.json())
+            .catch((error: Response) => Observable.throw(error.json()));
 }
 }
