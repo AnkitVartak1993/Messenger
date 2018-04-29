ad4e53ac99979c9aed5adc4861cc96d2d51c5072
AnkitVartak1993 <ankitvartak@gmail.com>
2018-04-29 13:07:57 -0400

    Added sign in route with JWT token for user

--- assets/app/auth/signin.component.ts
+++ assets/app/auth/signin.component.ts
@@ -1,15 +1,29 @@
 import { Component,  OnInit } from "@angular/core";
 import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { User } from "./user.model";
+import { AuthService } from "./auth.service";
+import { Router } from "@angular/router";
 
 @Component({
     selector: 'app-signin',
     templateUrl: './signin.component.html'
 })
 export class SigninComponent implements OnInit{
+    
 signInForm: FormGroup;
-
+constructor(private authService: AuthService, private router: Router){}
     onSubmit(){
-        console.log('form submitted');
+        //console.log('form submitted');
+        const user = new User(this.signInForm.value.email, this.signInForm.value.password);
+        this.authService.signin(user)
+            .subscribe(
+                data => {
+                    localStorage.setItem('token', data.token);
+                    localStorage.setItem('userId', data.userId);
+                    this.router.navigateByUrl('/');
+                },
+                error => console.error(error)
+            );
         this.signInForm.reset();
     }
     ngOnInit() {
