fcce09bb041063b978e2a0c640d3451e9e8aeb05
AnkitVartak1993 <ankitvartak@gmail.com>
2018-04-29 21:57:54 -0400

    Fixed template/TemplateURL issues

--- assets/app/messages/messages.component.ts
+++ assets/app/messages/messages.component.ts
@@ -2,7 +2,7 @@ import { Component} from "@angular/core";
 
 @Component({
     selector: 'app-messages',
-    templateUrl: `
+    template: `
         <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <app-message-input> </app-message-input>
