34ccdd3e8ea8f82f1d9b80daed6b9a567f62529d
AnkitVartak1993 <ankitvartak@gmail.com>
2018-04-29 21:45:32 -0400

    Added lazy-loading for AuthModule

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -5,9 +5,6 @@ import { AppComponent } from "./app.component";
 import { AuthenticationComponent } from "./auth/authentication.component";
 import { HeaderComponent } from "./shared/header.component";
 import { routing } from "./app.routing";
-import { LogoutComponent } from "./auth/logout.component";
-import { SigninComponent } from "./auth/signin.component";
-import { SignupComponent } from "./auth/signup.component";
 import { HttpModule } from "@angular/http";
 import { AuthService } from "./auth/auth.service";
 import { ErrorComponent } from "./errors/error.component";
@@ -18,9 +15,6 @@ import { MessageModule } from "./messages/message.module";
         AppComponent,
         AuthenticationComponent,
         HeaderComponent,
-        LogoutComponent,
-        SigninComponent,
-        SignupComponent,
         ErrorComponent
     ],
     imports: [BrowserModule, 
